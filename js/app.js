a8a68e418e99026a2202fba8edefb61c18dc426b
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-03-03 20:05:47 +0600

    Dynamic Data Loaded for All Cards

--- js/app.js
+++ js/app.js
@@ -1,35 +1,50 @@
-function loadTool(){
-    fetch('https://openapi.programming-hero.com/api/ai/tool/01')
-    .then(res => res.json())
-    .then(data => displayTools(data))
+const loadTool = async (id) =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayTools(data.data.tools);
 }
 
-const displayTools = Tools =>{
-    console.log(Tools.data);
-    const ToolsContainer = document.getElementById('tools-container');
-    // Tools.forEach(Tool =>
-    //     console.log(Tool)
-    // )
-    const ToolDiv = document.createElement('div');
-    ToolDiv.classList.add('col');
-    ToolDiv.innerHTML = `
-        <div class="card h-100">
-            <img src="${Tools.data.image_link[1]}" class="card-img-top" alt="...">
+const displayTools = tools =>{
+    const toolsContainer = document.getElementById('tools-container');
+    let tool = tools[0];
+    console.log(tool);
+    tools.forEach(tool =>{
+        const toolDiv = document.createElement('div');
+        toolDiv.classList.add('col');
+        toolDiv.innerHTML = `
+            <div class="card h-100">
+            <img src="${tool.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h4 class="card-text">Features</h4>
                 <ol>
-                <li>${Tools.data.features[1].feature_name}</li>
-                <li>${Tools.data.features[2].feature_name}</li>
-                <li>${Tools.data.features[3].feature_name}</li>
+                <li>${tool.features[0]}</li>
+                <li>${tool.features[1]}</li>
+                <li>${tool.features[2]}</li>
                 </ol>
             </div>
-        <div class="card-footer">
-            <h5 class="card-title">${Tools.data.tool_name}</h5>
-            <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-        `
-    ToolsContainer.appendChild(ToolDiv);
-// })
+            <hr class="w-80 mx-3">
+            <div class="mx-3 pb-3 d-flex justify-content-between align-items-center">
+            <div>
+            <h5 class="card-title">${tool.name}</h5>
+            <p><i class="fa-regular fa-calendar-days"></i>&nbsp;&nbsp;${tool.published_in}</p>
+            </div>
+            <div>
+            <i class="fs-2 fa-regular fa-circle-right"></i>
+            </div>
+            </div>
+            </div>
+            `;
+        toolsContainer.appendChild(toolDiv);
+    })
 }
+    // Tools.forEach(Tool =>
+    //     console.log(Tool)
+    // )
+  /*   
+        `
+    
+// })
 
-loadTool();
+ */
+loadTool(11);
