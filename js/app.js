59925e01168fc1978b481e18873a22e35a824a81
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-03-04 17:50:25 +0600

    Sort By Date Functionality Added

--- js/app.js
+++ js/app.js
@@ -40,7 +40,56 @@ const displayTools = tools =>{
         toolsContainer.appendChild(toolDiv);
     });
 
+    const sorting = (a, b)=>{
+        const dateA = new Date(a.published_in);
+        const dateB = new Date(b.published_in);
+        if(dateA > dateB){
+            return 1;
+        }
+        else if(dateA < dateB){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    };
+    
+    const sortedData = tools.sort(sorting);
+    
+    document.getElementById('sort-by-date').addEventListener('click', function(){
+        console.log(sortedData);
+        toolsContainer.innerHTML = '';
+        tools.forEach(tool =>{
+        const toolSortedDiv = document.createElement('div');
+        toolSortedDiv.classList.add('col');
+        toolSortedDiv.innerHTML = `
+            <div class="card h-100">
+            <img src="${tool.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-text">Features</h4>
+                <ol>
+                <li>${tool.features[0]}</li>
+                <li>${tool.features[1]}</li>
+                <li>${tool.features[2]}</li>
+                </ol>
+            </div>
+            <hr class="w-80 mx-3">
+            <div class="mx-3 pb-3 d-flex justify-content-between align-items-center">
+            <div>
+            <h5 class="card-title">${tool.name}</h5>
+            <p><i class="fa-regular fa-calendar-days"></i>&nbsp;&nbsp;${tool.published_in}</p>
+            </div>
+            <div>
+            <button onclick="loadToolDetails('${tool.id}')" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalPopup"><i class="fs-2 fa-regular fa-circle-right"></i></button>
+            </div>
+            </div>
+            </div>
+            `;
+            toolsContainer.appendChild(toolSortedDiv);
+    })
+})
 }
+
 
 // modal popup details
 const loadToolDetails = (id) =>{
@@ -51,7 +100,6 @@ const loadToolDetails = (id) =>{
 }
 
 const displayToolDetails = tool =>{
-    console.log(tool);
     const modalContent = document.getElementById('modal-content-inside');
         modalContent.innerHTML = `
         <button type="button" class="position-absolute top-0 end-0 translate-middle btn-close bg-danger p-2" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -106,21 +154,54 @@ const displayToolDetails = tool =>{
         </div>`;
 }
 
+
 /* 
 const sortDate = () =>{
     const url3 = `https://openapi.programming-hero.com/api/ai/tools`;
     fetch(url3)
     .then(res => res.json())
-    .then(data => sortDateAscending(data.data.tools[0].published_in))
+    .then(data => customSort(data.data.tools))
 }
 
-const sortDateAscending = publishedIn =>{
-    console.log(publishedIn);
-    const publishedIn = document.getElementById('tools-container');
-    
-    const date = publishedIn;
-    
-    
+customSort = tools =>{
+    const dateA = new Date(tools[0].published_in);
+    const dateB = new Date(tools[1].published_in);
+    const dateC = new Date(tools[3].published_in);
+    const dateD = new Date(tools[4].published_in);
+    const dateE = new Date(tools[5].published_in);
+    const dateF = new Date(tools[6].published_in);
+    const dateG = new Date(tools[7].published_in);
+    const dateH = new Date(tools[8].published_in);
+    const dateI = new Date(tools[9].published_in);
+    const dateJ = new Date(tools[10].published_in);
+    const dateK = new Date(tools[11].published_in);
+    // const dateL = new Date(tools[12].published_in);
+    console.log(dateA, dateB, dateC);
+    let sortedProducts = tools.sort(
+        (dateA, dateB) => 
+        (dateC < dateB) ? 1 : (dateC > dateB) ? -1 : 0);
+    console.log(sortedProducts);
+    if (dateA < dateB) {
+        console.log('dateB is greater');
+        return 1;
+    }
+    else if (dateA > dateB) {
+        console.log('dateA is greater)');
+        return -1;
+    }
+    else{
+        console.log('They are equal');
+        return 0;
+    }
+}
+ */
+// displayTools.sort(customSort);
+// var res = myarray.sort((dateA, dateB) => dateA - dateB);
+// console.log(res);
+
+// myarray.sort((a,b) => b.age - a.age)
+
+    /*
     date.sort(function(a, b){
         return a - b 
     });
@@ -132,15 +213,8 @@ const sortDateAscending = publishedIn =>{
     // console.log(publishedDate.sort((a,b) => isDescending ? new Date(b).getTime() - new Date(a).getTime() : new Date(a).getTime() - new Date(b).getTime()));
 
 
-
-
-// document.getElementById('sort-date').addEventListener('click', function(){
-//     toggleSpinner(true);
-//     });
-
-
     
-
+// Spinner to show on Loading Screen
 const toggleSpinner = isLoading => {
 const loaderSection = document.getElementById('spinner');
     if(isLoading){
@@ -150,6 +224,7 @@ const loaderSection = document.getElementById('spinner');
         loaderSection.classList.add('d-none');
     }
 }
+
 
 loadTool();
 
